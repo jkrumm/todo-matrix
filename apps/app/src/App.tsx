d46c2c319eb717c09e7c4c860ef94fb7568fff39
jkrumm <johannes.krumm@iu.org>
2023-11-04 14:34:21 +0100

    feat(create-turbo): moved tsconfig to config

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -14,7 +14,7 @@ const App = (): ReactElement => {
     <>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
+        <button onClick={(): void => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
